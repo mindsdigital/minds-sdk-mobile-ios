9770a2adf0c6a71403038f3735734930cb39e204
Liviu-Paul Bosbiciu <bosbiciu.liviu@gmail.com>
2022-04-06 12:01:10 +0300

    Create the success view

---
+++ Sources/minds-sdk-mobile-ios/views/SuccessView.swift
@@ -0,0 +1,57 @@
+//
+//  SuccessView.swift
+//
+//
+//  Created by Liviu Bosbiciu on 06.04.2022.
+//
+
+import SwiftUI
+
+@available(macOS 11, *)
+@available(iOS 13.0, *)
+public struct SuccessView: View {
+    @ObservedObject var uiMessagesSdk: MindsSDKUIMessages = MindsSDKUIMessages.shared
+    @ObservedObject var uiConfigSdk = MindsSDKUIConfig.shared
+    
+    public init() {
+        
+    }
+    
+    public var body: some View {
+        ZStack {
+            VStack {
+                Spacer()
+                Text(uiMessagesSdk.successMessageTitle)
+                    .foregroundColor(uiConfigSdk.textColor)
+//                    .font(uiConfigSdk.fontFamily.isEmpty ?
+//                            .custom(size: uiConfigSdk.)
+//                          : .custom("Georgia", size: 24, relativeTo: .title))
+                Text(uiMessagesSdk.successMessageBody)
+                    .foregroundColor(uiConfigSdk.textColor)
+                Spacer()
+            }
+            
+            Button(action: {
+                
+            }) {
+                Text(uiMessagesSdk.successButtonLabel)
+                    .frame(maxWidth: .infinity, maxHeight: 40)
+            }
+            .fillButtonStyle(backgroundColor: Color(.systemRed))
+            .frame(maxHeight: .infinity, alignment: .bottom)
+        }
+        .padding()
+    }
+}
+
+@available(macOS 11, *)
+@available(iOS 13.0, *)
+struct SuccessView_Previews: PreviewProvider {
+    static var previews: some View {
+        let uiMessagesSdk = MindsSDKUIMessages.shared
+        uiMessagesSdk.successMessageTitle = "Tudo certo!"
+        uiMessagesSdk.successMessageBody = "Biometria por voz registrada com sucesso."
+        uiMessagesSdk.successButtonLabel = "Continuar"
+        return SuccessView()
+    }
+}
