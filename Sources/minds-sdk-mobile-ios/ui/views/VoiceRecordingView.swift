c8cf202a837639166373a592fa0f9ddbe4bbd1b0
Vinícius Salmont <vinicius@salmont.dev>
2022-05-29 13:35:39 -0300

    fixing

--- Sources/minds-sdk-mobile-ios/ui/views/VoiceRecordingView.swift
+++ Sources/minds-sdk-mobile-ios/ui/views/VoiceRecordingView.swift
@@ -220,7 +220,7 @@ public struct VoiceRecordingView: View {
                                         } else {
                                             guard response.status != "invalid_length" else {
                                                 self.invalidLength = true
-                                                currentScreen = .main
+                                                currentScreen = .error
                                                 return
                                             }
 
