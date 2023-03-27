cee7968211dcca59912b1df40142817806659cfe
Wennys Camilo <wennys.camilo@mindsdigital.net>
2023-03-27 12:18:36 -0300

    parameterization animations color

--- Sources/MindsSDK/UI/View/LoadingView/LoadingView.swift
+++ Sources/MindsSDK/UI/View/LoadingView/LoadingView.swift
@@ -13,9 +13,17 @@ final class LoadingView: UIView {
     private lazy var lottieView: AnimationView = {
         $0.translatesAutoresizingMaskIntoConstraints = false
         let animation = Animation.named(LottieAnimations.loadingLottieAnimation, bundle: Bundle.module)
+        let keypath = AnimationKeypath(keypath: "Shape Layer 1.Ellipse 1.Stroke 1.Color")
         $0.animation = animation
         $0.contentMode = .scaleAspectFit
         $0.loopMode = .loop
+        if let (red, green, blue) = UIColor.hexToRGB(MindsSDKConfigs.shared.loadingLootieAnimationColor()) {
+            let colorProvider = ColorValueProvider(Color(r: (Double(red)/255), g: (Double(green)/255), b: (Double(blue)/255), a: 1))
+            $0.setValueProvider(colorProvider, keypath: keypath)
+        } else {
+            debugPrint("Invalid hex string")
+        }
+        $0.logHierarchyKeypaths()
         $0.play()
         return $0
     }(AnimationView(frame: .zero))
